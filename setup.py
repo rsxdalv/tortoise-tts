a84f57bbfaad800aad03ff76f6861f04904ffc58
Roberts Slisans <roberts.slisans@gmail.com>
2025-03-17 19:38:22 +0200

    enable transformers 4.49.0 compatiblity

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="tortoise-tts",
     packages=setuptools.find_packages(),
-    version="3.0.0",
+    version="3.0.1",
     author="James Betker",
     author_email="james@adamant.ai",
     description="A high quality multi-voice text-to-speech library",
@@ -27,7 +27,7 @@ setuptools.setup(
         'unidecode',
         'scipy',
         'librosa',
-        'transformers>=4.43.0,<4.50.0',
+        'transformers>=4.43.0',
         'tokenizers', # ==0.13.4.rc3',
         # 'scipy==1.13.1'
         # 'deepspeed==0.8.3',
